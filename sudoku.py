56070ef5617092f739231775065213a89fa610fa
Victor de Moura <victordemoura0409@gmail.com>
2024-10-22 10:49:25 -0300

    pronto

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,11 @@
 def compara(array):
+    # Para cada valor do array recebido
     for x in array:
+        # Se for diferente de '.' (vazio)
         if x != '.':
+            # Conte quantas vezes ele se repete dentro do array
             repet = array.count(x)
+            # Se houver repetição
             if repet > 1:
                 invalido()
 
@@ -13,13 +17,21 @@ def invalido():
 
 # Desenha a matriz
 def desenhaMatriz():
+    # Para cada linha
     for i in range(9):
+        # Inicie ela com | sem quebrar a linha
         print('|', end=' ')
+        # Para cada valor da linha
         for ii in range(9):
+            # Se for equivalente a vazio
             if matriz[i][ii] == '.':
+                # Digite vazio sem quebrar linha
                 print(' ', end='  ')
+            # Senão
             else:
+                # Digite o valor e não quebre linha
                 print(matriz[i][ii], end='  ')
+        # Finalize a linha com |
         print('|\n')
 
 
@@ -44,35 +56,49 @@ def recebeLinhas():
 
 
 def verificaEntrada():
+    # Para cada linha do array
     for i in range(9):
+        # Pegue o valor na coluna ii
         for ii in range(9):
+            # Verifique se o valor é válido
             if matriz[i][ii] not in '123456789.':
                 invalido()
 
 
 def comparaLinhas():
+    # Para cada linha
     for i in range(9):
+        # Transforme essa linha num novo array
         linha = matriz[i]
+        # Verifica repetição
         compara(linha)
 
 
 def comparaColunas():
+    # Para cada coluna
     for ii in range(9):
         coluna = []
-        # For que junta os números a serem comparados
+        # Pegue todos os valores da coluna
         for i in range(9):
+            # Coloque eles num array
             coluna.append(matriz[i][ii])
-        # For que faz a comparação
+        # Verifica repetição
         compara(coluna)
 
 
 def comparaBlocos():
+    # Vá de 3 em 3 linhas
     for i in range(0, 9, 3):
+        # E de 3 em 3 colunas
         for ii in range(0, 9, 3):
             bloco = []
+            # Para cada linha dessas 3 linhas
             for iii in range(3):
+                # E cada valor dessas linhas
                 for iiii in range(3):
+                    # Coloque eles numa matriz
                     bloco.append(matriz[i+iii][ii+iiii])
+            # Verifica repetição
             compara(bloco)
 
 
